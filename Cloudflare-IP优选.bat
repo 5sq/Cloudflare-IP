6a01b064fe89ef32457f7759d1ae50c1c0cf96f3
瓦斯曲 <wsq.weasel@gmail.com>
2023-07-30 11:48:26 +0800

    Update Cloudflare-IP优选.bat

--- Cloudflare-IP优选.bat
+++ Cloudflare-IP优选.bat
@@ -1,10 +1,17 @@
 @echo off
 setlocal enabledelayedexpansion
-set tph=%tmp%\cfip\
+set /a numN=%random%
+set tph=%tmp%\%numN%\
 if EXIST %tph% rd /s /q %tph%
 mkdir %tph%
-echo ==================================================================
+::==================================================================
+rem 可以增加或删除你要解析的域名
+Set CFname=www.cloudflare.com dash.cloudflare.com blog.cloudflare.com
+rem 可以自定义下面的测试端口
+set CFipPort=443
+::==================================================================
 Call :dnsip
+call :N个IP %tph%ip.txt
 echo ==================================================================
 Call :pings  %tph%ip.txt
 call :N个IP %tph%ip.txt
@@ -12,21 +19,21 @@ echo ping 通的 %countIP% 个IP。
 echo ==================================================================
 call :取前N个 %tph%ip.txt 32
 Call :CFPort %tph%ip.txt
-type %tph%ip.txt
 echo ==================================================================
 Call :N个IP %tph%ip.txt
 echo  CFIP 443端口测试成功的 %countIP% 个IP
 TITLE CFIP 443端口测试成功的 %countIP% 个IP
 echo ==================================================================
+rd /s /q %tph%
 pause
 goto :eof
 
 :dnsip
+echo ==================================================================
 set /a n=0
-Set CFname=www.cloudflare.com dash.cloudflare.com blog.cloudflare.com
 type nul >%tph%ip.txt
 for %%a in (%CFname%) do (
-Call :nslookupIP %%a&&set /a n+=1&&echo !n!、 DNS查询到的 %%a IP段 !ip12!
+Call :nslookupIP %%a&&set /a n+=1&&echo DNS查询到的 %%a IP段 !ip12!
 Set ip31=0&Set /a ip32=1+255&Call :生成IP 1
 )
 goto :eof
@@ -34,15 +41,13 @@ goto :eof
 :pings
 type nul >%tph%temp.csv
 set /a n=0
-for /f %%i in ('type %1') do (ping -n 1 -w 20 %%i | find "时间=" >> %tph%temp.csv&&set /a n+=1&&TITLE  ping IP：%%i/!n!)
+for /f %%i in ('type %1') do (ping -n 1 -w 20 %%i | find "时间=" >> %tph%temp.csv&&set /a n+=1&&TITLE  ping %countIP% 个IP：%%i/!n!)
 type nul > %1
 for /f "tokens=1-5 delims= " %%a in (%tph%temp.csv) do (echo %%a,%%b,%%c,%%d,%%e>>%1)
 type nul >%tph%temp.csv
 for /f "tokens=1-5 delims=," %%a in (%1) do (echo %%e,%%b,%%d>>%tph%temp.csv)
-sort %tph%temp.csv >%1
-type nul >%tph%temp.csv
-for /f "tokens=1-4 delims=," %%a in (%1) do (echo %%b,	%%a>>%tph%temp.csv)
-type %tph%temp.csv >%1
+sort %tph%temp.csv >%tph%temp1.csv&type nul >%1
+for /f "tokens=1-4 delims=," %%a in (%tph%temp1.csv) do (echo %%b,	%%a>>%1)
 goto :eof
 
 :取前N个
@@ -84,7 +89,7 @@ type nul > %tph%_tmp.txt
 set /a men=0&set /a men1=0
 for /f "tokens=1-2 delims=," %%a in (%1) do (
 set /a men1+=1
-tcping -n 1 -w 0.22 %%a 443 >NUL&&echo %%a,%%b>>%tph%_tmp.txt&&set /a men+=1&&TITLE 已测试：!men!/%countIP% 连接成功：!men!
+tcping -n 1 -w 0.22 %%a %CFipPort% >NUL&&echo %%a,	%CFipPort%,%%b&&echo %%a,%%b>>%tph%_tmp.txt&&set /a men+=1&&TITLE 已测试：!men!/%countIP% 连接成功：!men!
 if !men1! GEQ 32 timeout /T 5 /NOBREAK >NUL 2>NUL&&set /a men1=1
 )
 type %tph%_tmp.txt > %1
